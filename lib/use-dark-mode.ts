90c840592fdc7af9ac6769243a7790505e754f3d
MURHAF-ELMASRI <murhaf.msri@gmail.com>
2023-04-01 17:40:32 +0300

    add dark mode hooks to the app

--- lib/use-dark-mode.ts
+++ lib/use-dark-mode.ts
@@ -1,7 +1,22 @@
+import { useEffect } from 'react'
+
 import useDarkModeImpl from '@fisch0920/use-dark-mode'
 
+import { defaultMode } from './config'
+
 export function useDarkMode() {
-  const darkMode = useDarkModeImpl(false, { classNameDark: 'dark-mode' })
+  const darkMode = useDarkModeImpl(defaultMode === 'dark', {
+    classNameDark: 'dark-mode'
+  })
+  //workaround for the bug in useDarkModeImpl
+  useEffect(() => {    
+    if (defaultMode === 'dark') {
+      darkMode.enable()
+    } else {
+      darkMode.disable()
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   return {
     isDarkMode: darkMode.value,
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,6 +11,7 @@ import posthog from 'posthog-js'
 import 'prismjs/themes/prism-coy.css'
 // core styles shared by all of react-notion-x (required)
 import 'react-notion-x/src/styles.css'
+import 'styles/custom.css'
 import 'styles/global.css'
 // this might be better for dark mode
 // import 'prismjs/themes/prism-okaidia.css'
@@ -19,18 +20,7 @@ import 'styles/notion.css'
 // global style overrides for prism theme (optional)
 import 'styles/prism-theme.css'
 
-import { bootstrap } from '@/lib/bootstrap-client'
-import {
-  fathomConfig,
-  fathomId,
-  isServer,
-  posthogConfig,
-  posthogId
-} from '@/lib/config'
-
-if (!isServer) {
-  bootstrap()
-}
+import { fathomConfig, fathomId, posthogConfig, posthogId } from '@/lib/config'
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter()
